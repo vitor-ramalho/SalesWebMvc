cac90067037440649774117106dddb0abe892a2c
Vitor Ramalho <vitorrvilela@hotmail.com>
2020-10-24 11:45:44 -0300

    First Model-Controller-View - Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
